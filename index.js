ac4113fcbeba802b4e89596a2f37655e099643ef
jkwng <jkwng@google.com>
2021-03-19 14:11:43 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,99 @@
+const {Storage} = require('@google-cloud/storage');
+const https = require('https');
+
+// Creates a client
+const storage = new Storage();
+// Creates a client from a Google service account key.
+// const storage = new Storage({keyFilename: "key.json"});
+// For more information on ways to initialize Storage, please see https://googleapis.dev/nodejs/storage/latest/Storage.html
+
+const GOOGLE_STORAGE_URL = 'storage.googleapis.com';
+const GOOGLE_STORAGE_PORT = 443;
+const GOOGLE_BUCKET_NAME = 'fruishop-spa';
+
+async function sendIndexHtml(res) {
+  const bucket = storage.bucket(GOOGLE_BUCKET_NAME);
+
+  const [bucketMetadata] = await bucket.getMetadata();
+
+  const index_html = bucket.file(bucketMetadata.website.mainPageSuffix); 
+
+  https.get({
+    hostname: GOOGLE_STORAGE_URL,
+    port: GOOGLE_STORAGE_PORT,
+    path: GOOGLE_BUCKET_NAME + '/' + bucketMetadata.website.mainPageSuffix,
+  }, ((proxyresp) => {
+    //console.log(proxyresp);
+
+    // always return 200
+    res.status(200).set(proxyresp.headers)
+
+    proxyresp
+      .on('err', (err) => {
+        if (err.code == 404) {
+          sendIndexHtml(res);
+        } else {
+          console.error(err);
+        }
+      })
+      .on('end', () => {
+        res.end();
+      })
+      .pipe(res);
+  }))
+}
+
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.gcsproxy = (req, res) => {
+  //console.log("path to proxy: " + req.path);
+
+  // chop off any leading /'s
+  const path  = req.path.replace(/^\/*/, '');
+  //console.log("find file: " + path);
+
+  if (path === '') {
+    sendIndexHtml(res);
+    return;
+  }
+
+  https.get({
+    hostname: GOOGLE_STORAGE_URL,
+    port: GOOGLE_STORAGE_PORT,
+    path: GOOGLE_BUCKET_NAME +  req.path,
+  }, ((proxyresp) => {
+    // always return 200
+    res.status(200).set(proxyresp.headers)
+
+    proxyresp
+      .on('err', (err) => {
+        if (err.code == 404) {
+          // send 200 with main webpage if the path doesn't exist
+          sendIndexHtml(res);
+        } else {
+          // some other error happened
+          console.error(err);
+        }
+      })
+      .on('end', () => {
+        res.end();
+      })
+      .pipe(res);
+  }))
+};
+
+// standalone serving for testing below
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', exports.gcsproxy);
+app.get('/*', exports.gcsproxy);
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
